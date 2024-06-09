306d5c0f58a33eac9533738500d4cf4336399d0a
Ficow Shen <ficowshen@hotmail.com>
2024-06-09 16:56:21 +0800

    Use dependency lib SnapKit inside FicowPod;

--- PackPodAsXCFrameworkDemo/FicowPod/Classes/FicowPod.swift
+++ PackPodAsXCFrameworkDemo/FicowPod/Classes/FicowPod.swift
@@ -1,5 +1,11 @@
+import UIKit
+import SnapKit
+
 public class FicowPodEntry {
+
     public static func hello() {
-        print("Hello from FicowPod!")
+        let demoView = UIView()
+        demoView.snp.makeConstraints { $0.size.equalTo(1) } // use dependency lib SnapKit
+        print("Hello from FicowPod!") // should see the output in console if this method was called
     }
 }
